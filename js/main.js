ed132028485517b3e259ab0c92a959810bb57eab
Sooryakiran <sooryakiranpallikulathi@gmail.com>
2021-01-24 00:11:40 +0530

    Hero section

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function setup(){
+    console.log("Starting");
+    mobile = detectMob();
+    console.log(mobile);
+    // var mobile_url = "m/";
+    if(mobile)
+        document.getElementById("box").classList.remove('container');
+    
+
+}
+
+function detectMob() {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+
+    return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem);
+    });
+}
+
